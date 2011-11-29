1a413faac5fb60b29f02ac02c757d3931cc5c497
ronen barzel <ronen@barzel.org>
2011-11-29 23:36:32 +0000

    minor code cleanups.
    (get rid of unnecessary local variable, make sure to use sym instead of string for instance_variable_set.)

--- lib/key_struct.rb
+++ lib/key_struct.rb
@@ -17,23 +17,22 @@ module KeyStruct
   private
 
   def self.define_key_struct(access, keys) 
-    klass = Class.new
-    klass.class_eval do
+    Class.new.class_eval do
       keyvalues = Hash[*keys.map{|key| (Hash === key) ? key.to_a : [key, nil]}.flatten(2)]
       keys = keyvalues.keys
       send access, *keys
       define_method(:initialize) do |args={}|
         args = keyvalues.merge(args)
         keys.each do |key|
-          instance_variable_set("@#{key}", args.delete(key))
+          instance_variable_set("@#{key}".to_sym, args.delete(key))
         end
         raise ArgumentError, "Invalid argument(s): #{args.keys.map(&:inspect).join(' ')}; KeyStruct accepts #{keys.map(&:inspect).join(' ')}" if args.any?
       end
       define_method(:==) do |other|
         keys.all?{|key| self.send(key) == other.send(key)}
       end
+      self
     end
-    klass
   end
 
 end
