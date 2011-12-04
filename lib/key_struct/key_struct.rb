0a98c136f2f163b32285c64ba8177e6ecd3a678e
ronen barzel <ronen@barzel.org>
2011-12-04 22:40:17 +0000

    move main into definition key_struct/key_struct.rb

---
+++ lib/key_struct/key_struct.rb
@@ -0,0 +1,37 @@
+module KeyStruct
+
+  def self.reader(*keys)
+    define_key_struct(:attr_reader, keys)
+  end
+
+  def self.accessor(*keys)
+    define_key_struct(:attr_accessor, keys)
+  end
+
+  instance_eval do
+    alias :[] :accessor
+  end
+
+  private
+
+  def self.define_key_struct(access, keys) 
+    Class.new.class_eval do
+      keyvalues = Hash[*keys.map{|key| (Hash === key) ? key.to_a : [key, nil]}.flatten(2)]
+      keys = keyvalues.keys
+      send access, *keys
+      define_method(:initialize) do |args={}|
+        args = keyvalues.merge(args)
+        keys.each do |key|
+          instance_variable_set("@#{key}".to_sym, args.delete(key))
+        end
+        raise ArgumentError, "Invalid argument(s): #{args.keys.map(&:inspect).join(' ')}; KeyStruct accepts #{keys.map(&:inspect).join(' ')}" if args.any?
+      end
+      define_method(:==) do |other|
+        keys.all?{|key| self.send(key) == other.send(key)}
+      end
+      self
+    end
+  end
+
+end
+
