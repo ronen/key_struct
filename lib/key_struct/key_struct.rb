7b76b1eb7d48787ca3a0a5ff260f5ebfbc9c87e3
ronen barzel <ronen@barzel.org>
2012-05-15 01:10:53 +0100

    add Klass.key and Klass.defaults introspection

--- lib/key_struct/key_struct.rb
+++ lib/key_struct/key_struct.rb
@@ -15,21 +15,29 @@ module KeyStruct
   private
 
   def self.define_key_struct(access, keys) 
-    Class.new.class_eval do
+
+    defaults = (Hash === keys.last) ? keys.pop : {}
+    keys += defaults.keys
+
+    Class.new.tap{ |klass| klass.class_eval do
       include Comparable
-      keyvalues = Hash[*keys.map{|key| (Hash === key) ? key.to_a : [key, nil]}.flatten(2)]
-      keys = keyvalues.keys
       send access, *keys
+
+      define_singleton_method(:keys) { keys }
+      define_singleton_method(:defaults) { defaults }
+
       define_method(:initialize) do |args={}|
-        args = keyvalues.merge(args)
+        args = defaults.merge(args)
         keys.each do |key|
           instance_variable_set("@#{key}".to_sym, args.delete(key))
         end
         raise ArgumentError, "Invalid argument(s): #{args.keys.map(&:inspect).join(' ')}; KeyStruct accepts #{keys.map(&:inspect).join(' ')}" if args.any?
       end
+
       define_method(:==) do |other|
         keys.all?{|key| other.respond_to?(key) and self.send(key) == other.send(key)}
       end
+
       define_method(:<=>) do |other|
         keys.each do |key|
           cmp = (self.send(key) <=> other.send(key))
@@ -37,17 +45,21 @@ module KeyStruct
         end
         0
       end
+
       define_method(:to_hash) do
         Hash[*keys.map{ |key| [key, self.send(key)]}.flatten(1)]
       end
+
       define_method(:to_s) do
         "[#{self.class.name} #{keys.map{|key| "#{key}:#{self.send(key)}"}.join(' ')}]"
       end
+
       define_method(:inspect) do
         "<#{self.class.name}:0x#{self.object_id.to_s(16)} #{keys.map{|key| "#{key}:#{self.send(key).inspect}"}.join(' ')}>"
       end
-      self
+
     end
+    }
   end
 
 end
